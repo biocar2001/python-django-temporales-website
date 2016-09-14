e78a581e6d252519a6b6af420c9a95530130125f
cbenito <cbenito@stratio.com>
2016-09-14 16:24:34 +0200

    first push

---
+++ temporales/personas/models.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Salario(models.Model):
+	edad = models.IntegerField('Edad salario')
+	amount = models.IntegerField('salario bruto anual')
+
+class Oferta(models.Model):
+	hours_week = models.IntegerField('horas por semana')
+	time_contract = models.IntegerField('duracion contrato')
+	salarios = models.OneToManyField(Oferta, blank=True)	
+	payment_home = models.BooleanField('casa pagada', default=False)
+	payment_healthcare = models.BooleanField('seguro medico pagado', default=False)
+
+class Empresa(models.Model):
+	nombre = models.CharField('nombre', max_length=250)
+	ett = models.CharField('ett a la que pertenece', max_length=250)
+    localizacion = models.CharField('localizacion fisica de la empresa', max_length=500)
+    ofertas = models.OneToManyField(Oferta, blank=True)	
+
+class Persona(models.Model):
+	nombre = models.CharField('nombre', max_length=250)
+    apellidos = models.CharField('apellidos', max_length=500)
+    observaciones = models.TextField('observaciones', blank=True)
+    is_active = models.BooleanField('usuario asignado', default=False)
+    date_born = models.DateTimeField('fecha nacimiento')
+    english_level = models.IntegerField('nivel ingles')
+    empresa = models.OneToOneField(Empresa, blank=True)
+	
