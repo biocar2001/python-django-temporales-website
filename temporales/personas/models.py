74ff3827f9faacbdf354b23f66890fdf8cd60b5e
cbenito <cbenito@stratio.com>
2016-09-14 17:09:39 +0200

    Database creation

--- temporales/personas/models.py
+++ temporales/personas/models.py
@@ -12,22 +12,22 @@ class Salario(models.Model):
 class Oferta(models.Model):
 	hours_week = models.IntegerField('horas por semana')
 	time_contract = models.IntegerField('duracion contrato')
-	salarios = models.OneToManyField(Oferta, blank=True)	
+	salarios = models.ForeignKey(Salario)	
 	payment_home = models.BooleanField('casa pagada', default=False)
 	payment_healthcare = models.BooleanField('seguro medico pagado', default=False)
 
 class Empresa(models.Model):
 	nombre = models.CharField('nombre', max_length=250)
 	ett = models.CharField('ett a la que pertenece', max_length=250)
-    localizacion = models.CharField('localizacion fisica de la empresa', max_length=500)
-    ofertas = models.OneToManyField(Oferta, blank=True)	
+	localizacion = models.CharField('localizacion fisica de la empresa', max_length=500)
+	ofertas = models.ForeignKey(Oferta)	
 
 class Persona(models.Model):
 	nombre = models.CharField('nombre', max_length=250)
-    apellidos = models.CharField('apellidos', max_length=500)
-    observaciones = models.TextField('observaciones', blank=True)
-    is_active = models.BooleanField('usuario asignado', default=False)
-    date_born = models.DateTimeField('fecha nacimiento')
-    english_level = models.IntegerField('nivel ingles')
-    empresa = models.OneToOneField(Empresa, blank=True)
+	apellidos = models.CharField('apellidos', max_length=500)
+	observaciones = models.TextField('observaciones', blank=True)
+	is_active = models.BooleanField('usuario asignado', default=False)
+	date_born = models.DateTimeField('fecha nacimiento')
+	english_level = models.IntegerField('nivel ingles')
+	empresa = models.OneToOneField(Empresa, blank=True)
 	
--- temporales/temporales/settings.py
+++ temporales/temporales/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'temporales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'temporales',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
